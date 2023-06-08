fd6c121c99cab1ad8dd583209827362f7baec0c6
Drew Mclachlan <drew.mclachlan@icloud.com>
2023-06-08 00:32:22 -0400

    adding functionailty

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/fatih/color"
+)
+
+// Define log levels as constants
+const (
+	DEBUG = iota
+	INFO
+	WARN
+	ERROR
+)
+
+// Define the level mapping
+var levelMapping = map[string]int{
+	"DEBUG": DEBUG,
+	"INFO":  INFO,
+	"WARN":  WARN,
+	"ERROR": ERROR,
+}
+
+// Current logging level
+var level = DEBUG
+
+// Setting custom output to std out over print as its easier to test
+var Output io.Writer = os.Stdout
+
+// SetLevel function to set the current logging level
+func SetLevel(logLevel string) {
+	if val, exists := levelMapping[strings.ToUpper(logLevel)]; exists {
+		level = val
+	} else {
+		fmt.Fprintf(Output, "Invalid level: %s\n", logLevel)
+	}
+}
+
+// DisableColors disables colorized output
+func DisableColors() {
+	color.NoColor = true
+}
+
+// Debug log function
+func Debug(v ...interface{}) {
+	if level <= DEBUG {
+		fmt.Fprintf(color.Output, "[%s] %v\n", color.MagentaString("DEBUG"), strings.Join(formatArgs(v...), " "))
+	}
+}
+
+// Info log function
+func Info(v ...interface{}) {
+	if level <= INFO {
+		fmt.Fprintf(color.Output, "[%s] %v\n", color.CyanString("INFO"), strings.Join(formatArgs(v...), " "))
+	}
+}
+
+// Info log function with Success: in green before the log message
+func InfoWithSuccess(v ...interface{}) {
+	if level <= INFO {
+		fmt.Fprintf(color.Output, "[%s] %s: %v\n", color.CyanString("INFO"), color.GreenString("Success"), strings.Join(formatArgs(v...), " "))
+	}
+}
+
+// Warning log function
+func Warn(v ...interface{}) {
+	if level <= WARN {
+		fmt.Fprintf(color.Output, "[%s] %v\n", color.YellowString("WARN"), strings.Join(formatArgs(v...), " "))
+	}
+}
+
+// Error log function
+func Error(v ...interface{}) {
+	if level <= ERROR {
+		fmt.Fprintf(color.Output, "[%s] %v\n", color.RedString("ERROR"), strings.Join(formatArgs(v...), " "))
+	}
+}
+
+// Helper function to format log arguments as strings
+func formatArgs(v ...interface{}) []string {
+	formattedArgs := make([]string, len(v))
+	for i, arg := range v {
+		formattedArgs[i] = fmt.Sprintf("%v", arg)
+	}
+	return formattedArgs
+}
---
+++ main_test.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/fatih/color"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetLevelInvalid(t *testing.T) {
+	var buf bytes.Buffer
+	originalOutput := Output
+	Output = &buf                              // redirect output to buf
+	defer func() { Output = originalOutput }() // restore original output after the test
+
+	SetLevel("INVALID")
+	expected := "Invalid level: INVALID\n"
+	assert.Contains(t, buf.String(), expected, "Expected error message")
+}
+
+func TestSetLevelLowerCase(t *testing.T) {
+	SetLevel("info")
+	assert.Equal(t, INFO, level, "Expected level to be set to INFO")
+}
+
+func TestLogFunctions(t *testing.T) {
+	var buf bytes.Buffer
+	color.Output = &buf
+
+	// Set log level to INFO
+	level = INFO
+
+	// Call log functions
+	Debug("Debug message")
+	Info("Info message")
+	Warn("Warning message")
+	Error("Error message")
+
+	expected := "[INFO] Info message\n[WARN] Warning message\n[ERROR] Error message\n"
+	assert.Contains(t, buf.String(), expected, "Expected log output")
+}
+
+func TestInfoWithSuccess(t *testing.T) {
+	var buf bytes.Buffer
+	color.Output = &buf
+
+	// Set log level to INFO
+	level = INFO
+
+	InfoWithSuccess("Operation completed")
+
+	expected := "[INFO] " + color.GreenString("Success") + ": Operation completed\n"
+	assert.Contains(t, buf.String(), expected, "Expected log output")
+}
+
+func TestDisableColors(t *testing.T) {
+	var buf bytes.Buffer
+	color.Output = &buf
+	defer func() { Output = os.Stdout }()
+
+	DisableColors()
+	Info("Test message")
+
+	expected := "[INFO] Test message\n"
+	assert.Contains(t, buf.String(), expected, "Expected log output without color codes")
+}
+
+func TestSetLevelCaseInsensitive(t *testing.T) {
+	SetLevel("info")
+	assert.Equal(t, INFO, level, "Expected level to be set to INFO")
+
+	SetLevel("INFO")
+	assert.Equal(t, INFO, level, "Expected level to be set to INFO")
+
+	SetLevel("InFo")
+	assert.Equal(t, INFO, level, "Expected level to be set to INFO")
+}
+
+func TestInvalidLevelDoesNotChangeLevel(t *testing.T) {
+	SetLevel("INFO")
+	assert.Equal(t, INFO, level, "Expected level to be set to INFO")
+
+	SetLevel("INVALID")
+	assert.Equal(t, INFO, level, "Expected level to still be INFO after invalid SetLevel call")
+}
+
+func TestLogMultipleArguments(t *testing.T) {
+	var buf bytes.Buffer
+	color.Output = &buf
+	defer func() { Output = os.Stdout }()
+
+	level = INFO
+	Info("Test", "message", 123)
+
+	expected := "[INFO] Test message 123\n"
+	assert.Contains(t, buf.String(), expected, "Expected log output with multiple arguments")
+}
+
+func benchmarkLogLatency(logFunc func(...interface{}), b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		logFunc("Log message")
+	}
+}
+
+func BenchmarkCustomLog(b *testing.B) {
+	Info("Warm up")
+	b.ResetTimer()
+	benchmarkLogLatency(Info, b)
+}
+
+func BenchmarkStandardLog(b *testing.B) {
+	log.Println("Warm up")
+	b.ResetTimer()
+	benchmarkLogLatency(log.Println, b)
+}
